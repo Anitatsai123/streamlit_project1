94ab0bc68fcb53c2f841ebd486a084463637a892
Anitatsai123 <anitatsai123@gmail.com>
2022-11-11 15:54:59 +0800

    modify

--- index.py
+++ index.py
@@ -2,3 +2,21 @@ import streamlit as st
 st.title("這是我的第一個streamlit專案")
 st.header("請是我的次標題")
 st.subheader("這是我的次次標題")
+st.write("這是段落1")
+st.write("這是段落2")
+st.write("這是段落3")
+st.markdown('''
+---
+# H1
+---
+## H2
+---
+### H3
+---
+#### H4
+---
+##### H5
+---
+###### H6
+---
+''')
