5d9d5f7e606847ce5b7359eb76dba45893ea7c7f
Anitatsai123 <anitatsai123@gmail.com>
2022-11-15 10:39:57 +0800

    modify

--- index.py
+++ index.py
@@ -1,11 +1,31 @@
 import streamlit as st
+
+import time
+import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import time
+import json
+
+
+# Use a service account.
+app = None
+db = None
+if db is None :
+    print("建立firestore實體")
+    key_dict = json.loads(st.secrets["textkey"])
+    cred = credentials.Certificate(key_dict)
+
+    if app is None:
+        app = firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+
 
 st.title("光線和距離即時監控")
 
-def downloadData(): #function, not object method
+
+
+def downloadData():
     print("下載資料")
 
 
